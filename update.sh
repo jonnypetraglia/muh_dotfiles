1ef74781a973773355fcd53900cc8605fd6728e1
Jonny Petraglia <jpetraglia@inspirato.com>
2022-01-22 10:20:36 -0700

    Fixed Fish and Systemd backups

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
-# Updates the files in this repo.
+# Updates the files in this repo from the files in the current system.
+
+
+# Fish is pretty modular so some files might have been removed
+rm -rf home/notbryant/.config/fish && cp -r ~/.config/fish home/notbryant/.config/fish
+
+# Does a simple copy of all entities, i.e. only adds and does not delete
 find . -type f -not -path './.git/*' -not -path './update.sh' | xargs -I % sh -c 'cp $(echo % | cut -c2-) %'
